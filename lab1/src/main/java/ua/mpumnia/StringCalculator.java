d6aaca9ec491415dcdf4b17c06080b808f423f84
Mykhailo Pumnia <misha.pumnya@gmail.com>
2023-09-15 21:35:02 +0300

    Complete step 1

--- lab1/src/main/java/ua/mpumnia/StringCalculator.java
+++ lab1/src/main/java/ua/mpumnia/StringCalculator.java
@@ -1,4 +1,21 @@
 package ua.mpumnia;
 
 public class StringCalculator {
+
+    public int add(String numbers) {
+
+        if (numbers.length() == 0) {
+            return 0;
+        }
+
+        String[] nums = numbers.split(",");
+
+        int sum = 0;
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
+    }
+
 }
--- lab1/src/test/java/ua/mpumnia/StringCalculatorTest.java
+++ lab1/src/test/java/ua/mpumnia/StringCalculatorTest.java
@@ -1,4 +1,28 @@
 package ua.mpumnia;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class StringCalculatorTest {
+
+    private static final StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void emptyStringEqualToZero() {
+        int actual = calculator.add("");
+        Assertions.assertEquals(0, actual);
+    }
+
+    @Test
+    void singleNumberEqualToItself() {
+        int actual = calculator.add("12");
+        Assertions.assertEquals(12, actual);
+    }
+
+    @Test
+    void addTwoNumbersSeparatedWithComma() {
+        int actual = calculator.add("6,14");
+        Assertions.assertEquals(20, actual);
+    }
+
 }
